792a1247b021a5f47f6745692d3b6fc241d40673
Uendelighet <licglaura52@gmail.com>
2024-09-25 17:30:25 -0500

    eat: new component FilterButtonProps

--- Proyecto_Cookun/src/components/homeComponents/botonEtiqueta.ts/boton.ts
+++ Proyecto_Cookun/src/components/homeComponents/botonEtiqueta.ts/boton.ts
@@ -1,35 +1,53 @@
+
+export enum FilterButtonProps {
+	'img' = 'img',
+	'tittle' = 'tittle',
+}
+	
+
 class FilterButton extends HTMLElement {
+	img?: string;
+	tittle?: string;
+
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
 	}
 
 	static get observedAttributes() {
-		return ['image', 'title'];
+		const attrs: Record<FilterButtonProps, null> = {
+			img: null,
+			tittle: null,
+		};
+		return Object.keys(attrs);
 	}
 
 	connectedCallback() {
 		this.render();
 	}
 
-	attributeChangedCallback(propName, oldValue, newValue) {
-		if (oldValue !== newValue) {
-			this[propName] = newValue;
-			this.render();
+	attributeChangedCallback(propName: FilterButtonProps, oldValue: string | undefined, newValue: string | undefined) {
+		switch (propName) {
+			default:
+				this[propName] = newValue;
+				break;
 		}
+		this.render();
 	}
 
 	render() {
-		this.shadowRoot.innerHTML = `
-			<link rel="stylesheet" href="./src/components/myComponent/explore.css">
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = `
+				<link rel="stylesheet" href="Proyecto_Cookun/src/components/homeComponents/botonEtiqueta.ts/boton.css>
 			<button> 
-				<img class="portadas" src="${this.image}" alt="">
-				<h1>${this.title}</h1>
+				<img class="portadas" src="${this.img}" alt="">
+				<h1>${this.tittle}</h1>
 			</button>
-		`;
-		
+          `;
+		}
 	}
 }
 
 customElements.define('filter-button', FilterButton);
 export default FilterButton;
+
